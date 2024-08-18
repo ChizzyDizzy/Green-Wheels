524e9e7cb2803902c75b4c8e30a757c00dd73bd8
Chirath Sanduwara <cb011568@students.apiit.lk>
2024-08-18 11:54:52 +0530

    asfdf

--- admin_web_panel/lib/widgets/users_data_list.dart
+++ admin_web_panel/lib/widgets/users_data_list.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+
 import '../methods/common_methods.dart';
 
 
--- drivers_app/lib/main.dart
+++ drivers_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:users_app/pages/home_page.dart';
 import 'authentication/login_screen.dart';
@@ -40,13 +41,13 @@ class MyApp extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Drivers App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: FirebaseAuth.instance.currentUser == null ? LoginScreen() : HomePage(),
+      home: FirebaseAuth.instance.currentUser == null ? const LoginScreen() : const HomePage(),
     );
   }
 }
